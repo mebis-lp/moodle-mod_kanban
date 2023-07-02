ee803cc4dfbc9fc470d1178845da78df76cbedb2
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-07-02 19:52:34 +0200

    MBS-7743: German translation

---
+++ lang/de/kanban.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * German language file for mod_kanban - will be moved to AMOS once the plugin is approved.
+ *
+ * @package     mod_kanban
+ * @copyright   2023, ISB Bayern
+ * @author      Stefan Hanauska <stefan.hanauska@csg-in.de>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addcard'] = 'Füge eine Karte zu dieser Spalte hinzu';
+$string['addcolumn'] = 'Füge eine Spalte zu diesem Board hinzu';
+$string['assignee'] = 'Verantwortliche(r)';
+$string['assignees'] = 'Verantwortliche';
+$string['assignme'] = 'Mir zuweisen';
+$string['attachments'] = 'Anhänge';
+$string['cardtitle'] = 'Titel der Karte';
+$string['column'] = 'Spalte';
+$string['closecard'] = 'Karte abschließen';
+$string['deletecard'] = 'Karte löschen';
+$string['deletecardconfirm'] = 'Möchten Sie diese Karte wirklich löschen?';
+$string['deletecolumn'] = 'Spalte löschen';
+$string['deletecolumnconfirm'] = 'Möchten Sie diese Spalte wirklich löschen?';
+$string['doing'] = 'In Arbeit';
+$string['done'] = 'Erledigt';
+$string['duedate'] = 'Fälligkeitsdatum';
+$string['editcard'] = 'Karte bearbeiten';
+$string['editcolumn'] = 'Spalte bearbeiten';
+$string['editdetails'] = 'Details bearbeiten';
+$string['kanban:addcard'] = 'Eine Karte zu einem Board hinzufügen';
+$string['kanban:addinstance'] = 'Ein Board hinzufügen';
+$string['kanban:assignothers'] = 'Anderen eine Karte zuweisen';
+$string['kanban:assignself'] = 'Sich selbst eine Karte zuweisen';
+$string['kanban:editallboards'] = 'Alle Boards bearbeiten';
+$string['kanban:managecards'] = 'Die Karten auf dem Board bearbeiten';
+$string['kanban:managecolumns'] = 'Die Spalten auf dem Board bearbeiten';
+$string['kanban:moveallcards'] = 'Alle Karten verschieben';
+$string['kanban:moveassignedcards'] = 'Karten verschieben, denen man selbst zugewiesen ist';
+$string['kanban:view'] = 'Ein Board anzeigen';
+$string['kanban:viewallboards'] = 'Alle Boards anzeigen';
+$string['kanban:viewhistory'] = 'Den Verlauf des Boards anzeigen';
+$string['loading'] = 'Kanban-Board wird geladen';
+$string['lock'] = 'Sperren';
+$string['modulename'] = 'Kanban-Board';
+$string['modulenameplural'] = 'Kanban-Boards';
+$string['movecard'] = 'Karte verschieben';
+$string['movecolumn'] = 'Spalte verschieben';
+$string['name'] = 'Name des Boards';
+$string['name_help'] = 'Dieser Name wird in der Kursübersicht und als Titel des Boards sichtbar sein.';
+$string['newcard'] = 'Neue Karte';
+$string['newcolumn'] = 'Neue Spalte';
+$string['pluginadministration'] = 'Kanban-Administration';
+$string['pluginname'] = 'Kanban-Board';
+$string['reminderdate'] = 'Zeitpunkt für die Erinnerung';
+$string['showattachment'] = 'Anhänge anzeigen';
+$string['showdescription'] = 'Beschreibung anzeigen';
+$string['todo'] = 'Zu erledigen';
+$string['unassign'] = 'Zuweisung aufheben';
+$string['unassignme'] = 'Mir nicht mehr zuweisen';
+$string['uncomplete'] = 'Erneut öffnen';
+$string['unlock'] = 'Entsperren';
