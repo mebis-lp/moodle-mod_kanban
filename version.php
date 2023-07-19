50cd4e75a308ab5f7ec66a53100b4a5dfc4c2529
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-07-19 09:18:22 +0200

    MBS-7743: Change minimum moodle version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_kanban';
 $plugin->release = '0.1';
-$plugin->version = 2023071701;
+$plugin->version = 2023071901;
 $plugin->requires = 2022041900;
-$plugin->supported = [400, 402];
+$plugin->supported = [401, 403];
 $plugin->maturity = MATURITY_ALPHA;
