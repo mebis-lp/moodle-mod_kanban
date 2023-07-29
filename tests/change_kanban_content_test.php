3ca7fffa7163d80cf5150b449b8f085e54c113d4
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-07-29 09:37:13 +0200

    MBS-7743: external_api

--- tests/change_kanban_content_test.php
+++ tests/change_kanban_content_test.php
@@ -76,7 +76,7 @@ class change_kanban_content_test extends \advanced_testcase {
         $columnids = $DB->get_fieldset_select('kanban_column', 'id', 'kanban_board = :id', ['id' => $boardid]);
 
         $returnvalue = \mod_kanban\external\change_kanban_content::add_column($this->kanban->cmid, $boardid, ['aftercol' => 0]);
-        $returnvalue = external_api::clean_returnvalue(
+        $returnvalue = \external_api::clean_returnvalue(
             \mod_kanban\external\change_kanban_content::add_column_returns(),
             $returnvalue
         );
@@ -94,7 +94,7 @@ class change_kanban_content_test extends \advanced_testcase {
         $this->assertEquals(1, $DB->count_records('kanban_column', ['id' => $columnid]));
 
         $returnvalue = \mod_kanban\external\change_kanban_content::add_column($this->kanban->cmid, $boardid, ['aftercol' => 0]);
-        $returnvalue = external_api::clean_returnvalue(
+        $returnvalue = \external_api::clean_returnvalue(
             \mod_kanban\external\change_kanban_content::add_column_returns(),
             $returnvalue
         );
