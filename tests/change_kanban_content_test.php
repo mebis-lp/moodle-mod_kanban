6c6decb268f78bac9bc09beb223b6066b0e7bac5
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-07-29 11:01:42 +0200

    MBS-7743: Small fix

--- tests/change_kanban_content_test.php
+++ tests/change_kanban_content_test.php
@@ -148,12 +148,12 @@ class change_kanban_content_test extends \advanced_testcase {
         $this->assertCount(2, $update);
         $this->assertEquals('cards', $update[0]['name']);
         $this->assertEquals('columns', $update[1]['name']);
-        $cardid = $update[1]['fields']['id'];
+        $cardid = $update[0]['fields']['id'];
 
         $card = $boardmanager->get_card($cardid);
         $this->assertEquals('Testcard', $card->title);
-        $this->assertEquals($boardid, $update[1]['fields']['kanban_board']);
-        $this->assertEquals($columnid, $update[1]['fields']['kanban_column']);
+        $this->assertEquals($boardid, $update[0]['fields']['kanban_board']);
+        $this->assertEquals($columnid, $update[0]['fields']['kanban_column']);
         $this->assertEquals($cardid, $update[1]['fields']['sequence']);
 
         $returnvalue = \mod_kanban\external\change_kanban_content::add_card(
@@ -167,6 +167,7 @@ class change_kanban_content_test extends \advanced_testcase {
         );
 
         $update = json_decode($returnvalue['update'], true);
+        $card2id = $update[0]['fields']['id'];
         $this->assertCount(2, $update);
         $this->assertEquals('cards', $update[0]['name']);
         $this->assertEquals('columns', $update[1]['name']);
