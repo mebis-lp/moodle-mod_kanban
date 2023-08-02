da7f80a98b00c5d96a2ff4f1736f71ea9712c084
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-08-02 17:28:16 +0200

    MBS-7743: Add missing upgrade.php

---
+++ db/upgrade.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_kanban db upgrades.
+ *
+ * @package    mod_kanban
+ * @copyright  2023 ISB Bayern
+ * @author     Stefan Hanauska
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Define upgrade steps to be performed to upgrade the plugin from the old version to the current one.
+ *
+ * @param int $oldversion Version number the plugin is being upgraded from.
+ */
+function xmldb_kanban_upgrade($oldversion) {
+    // No upgrade steps until now.
+    return true;
+}
