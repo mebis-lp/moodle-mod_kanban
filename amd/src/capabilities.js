5d147098d7e93079e442e56a68f9effcbec22518
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-08-11 22:53:28 +0200

    MBS-7891: Add capabilities js class

---
+++ amd/src/capabilities.js
@@ -0,0 +1,16 @@
+/**
+ * Capabilities for mod_kanban.
+ */
+export default {
+    ADDCARD: 'addcard',
+    ASSIGNOTHERS: 'assignothers',
+    ASSIGNSELF: 'assignself',
+    EDITALLBOARDS: 'editallboards',
+    MANAGEBOARD: 'manageboard',
+    MANAGECARDS: 'managecards',
+    MANAGECOLUMNS: 'managecolumns',
+    MOVEALLCARDS: 'moveallcards',
+    MOVEASSIGNEDCARDS: 'moveassignedcards',
+    VIEWALLBOARDS: 'viewallboards',
+    VIEWHISTORY: 'viewhistory',
+};
