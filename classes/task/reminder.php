6f9815021fdd430f15e1bb9d641df5db412c7896
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-08-11 22:37:37 +0200

    MBS-7890: Fix reminder job for all DBMS (#9)

--- classes/task/reminder.php
+++ classes/task/reminder.php
@@ -51,17 +51,19 @@ class reminder extends \core\task\scheduled_task {
      */
     public function execute() {
         global $DB;
+        $time = time();
         $kanbancards = $DB->get_records_sql(
-            'SELECT ' . $DB->sql_concat('c.id', '"-"', 'a.userid') . ' as uniqid,
+            'SELECT ' . $DB->sql_concat('c.id', "'-'", 'a.userid') . ' as uniqid,
                     c.id as id, c.title as title, k.name as boardname, c.duedate as duedate, a.userid as userid, k.id as instance
                FROM {kanban_card} c
          INNER JOIN {kanban_assignee} a ON a.kanban_card = c.id
                 AND c.duedate != 0
                 AND c.reminder_sent = 0
                 AND c.completed = 0
-                AND (c.duedate < now() OR (c.reminderdate != 0 AND c.reminderdate < now()))
+                AND (c.duedate < :time OR (c.reminderdate != 0 AND c.reminderdate < :time2))
          INNER JOIN {kanban_board} b ON b.id = c.kanban_board
-         INNER JOIN {kanban} k ON b.kanban_instance = k.id'
+         INNER JOIN {kanban} k ON b.kanban_instance = k.id',
+            ['time' => $time, 'time2' => $time]
         );
         foreach ($kanbancards as $kanbancard) {
             list($course, $cminfo) = get_course_and_cm_from_instance($kanbancard->instance, 'kanban');
